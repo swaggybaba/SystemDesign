29572dc2ff29ba37bed710462ed773919d160a01
Prafful Chauhan <praffulchauhan49@gmail.com>
2023-09-13 09:02:23 +0530

    LLD of ChainOfResponsibility

---
+++ LLDesign/ChainOfResponsibility/LogProcessor.java
@@ -0,0 +1,20 @@
+package ChainOfResponsibility;
+
+public class LogProcessor {
+	
+	public static int INFO=1;
+	public static int DEBUG=2;
+	public static int ERROR=3;
+	
+	LogProcessor nextLogProcessor;
+	
+	public LogProcessor(LogProcessor logProcessor) {
+		nextLogProcessor=logProcessor;
+	}
+	
+	public void log(int logLevel, String message) {
+		if(nextLogProcessor!=null) {
+			nextLogProcessor.log(logLevel,message);
+		}
+	}
+}
